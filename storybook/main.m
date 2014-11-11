c877d8566bd92f633e388b3c913fd0fa20136d1d
Kevin Casey <kacasey@berkeley.edu>
2014-11-11 15:31:14 -0800

    Initial Commit

---
+++ storybook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  storybook
+//
+//  Created by Kevin Casey on 11/11/14.
+//  Copyright (c) 2014 ieor190. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
