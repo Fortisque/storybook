56775942a870cc32de57befa0a57c44a7afad7b9
Kevin Casey <kacasey@berkeley.edu>
2014-11-18 23:25:23 -0800

    drawing

--- storybook/DrawingPageViewController.h
+++ storybook/DrawingPageViewController.h
@@ -8,13 +8,26 @@
 
 #import <UIKit/UIKit.h>
 #import "BasePageViewController.h"
+#import "DrawingPrompterViewController.h"
 
-@interface DrawingPageViewController : BasePageViewController
+@interface DrawingPageViewController : BasePageViewController <UIGestureRecognizerDelegate> {
+    
+    CGPoint lastPoint;
+    CGFloat red;
+    CGFloat green;
+    CGFloat blue;
+    CGFloat brush;
+    CGFloat opacity;
+    BOOL mouseSwiped;
+}
+
 @property (strong, nonatomic) IBOutlet UIImageView *mainImage;
 @property (strong, nonatomic) IBOutlet UIImageView *tempDrawImage;
+@property (weak, nonatomic) DrawingPrompterViewController *presenter;
 
 - (IBAction)colorPressed:(id)sender;
 
 - (IBAction)eraserPressed:(id)sender;
+- (IBAction)donePressed:(id)sender;
 
 @end
---
+++ storybook/DrawingPrompterViewController.h
@@ -0,0 +1,15 @@
+//
+//  DrawingPrompterViewController.h
+//  storybook
+//
+//  Created by Kevin Casey on 11/18/14.
+//  Copyright (c) 2014 ieor190. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BasePageViewController.h"
+
+@interface DrawingPrompterViewController : BasePageViewController
+
+- (void)updateCustomImage:(UIImage *)image;
+@end
