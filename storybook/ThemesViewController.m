2861695c16daf7a553593f3c03addf59b322ff05
alice <aliceworkmail@gmail.com>
2014-11-26 21:35:35 -0800

    changed background color

--- storybook/ThemesViewController.m
+++ storybook/ThemesViewController.m
@@ -18,8 +18,20 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    _logoImage.image = [UIImage imageNamed:@"storybubblesF"];
-    [Animations spawnBubblesInView:self.view];
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    CGFloat SCREEN_WIDTH = screenRect.size.width;
+    CGFloat SCREEN_HEIGHT = screenRect.size.height;
+    
+    
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, SCREEN_WIDTH, 150)];
+    title.text = @"storybubbles";
+    title.font = [UIFont fontWithName:@"Quicksand-BoldItalic" size:100];
+    title.textAlignment = NSTextAlignmentCenter;
+    
+    [self.view addSubview:title];
+    
+    self.view.backgroundColor = [Helper colorWithHexString:@"00C7FF"];
+    //[Animations spawnBubblesInView:self.view];
 }
 
 - (void)didReceiveMemoryWarning {
